a6029edecaaa9a8f805581b66950baa6100b4c4c
WHENMANANCODES <mananjain564@gmail.com>
2026-02-25 10:36:36 +0530

    made some changes in mock data

--- src/data/mockData.js
+++ src/data/mockData.js
@@ -1,37 +1,36 @@
 const problemsData = [
-      {date : "1-02-2026",difficulty : "Easy"},
-      {date : "1-02-2026",difficulty : "Easy"},
-      {date : "1-02-2026",difficulty : "Easy"},
-      {date : "1-02-2026",difficulty : "Easy"},
-      {date : "1-02-2026",difficulty : "Easy"},
-      {date : "1-02-2026",difficulty : "Easy"},
-      {date : "1-02-2026",difficulty : "Easy"},
-      {date : "1-02-2026",difficulty : "Easy"},
-      {date : "1-02-2026",difficulty : "Easy"},
-      {date : "1-02-2026",difficulty : "hard"},
-      {date : "1-02-2026",difficulty : "Easy"},
-      {date : "1-02-2026",difficulty : "Easy"},
-      {date : "1-02-2026",difficulty : "Hard"},
-      {date : "1-02-2026",difficulty : "Easy"},
-      {date : "1-02-2026",difficulty : "hard"},
-      {date : "1-02-2026",difficulty : "Easy"},
-      {date : "1-02-2026",difficulty : "Easy"},
-      {date : "1-02-2026",difficulty : "Easy"},
-      {date : "1-02-2026",difficulty : "Easy"},
-      {date : "1-02-2026",difficulty : "Easy"},
-      {date : "1-02-2026",difficulty : "Easy"},
-      {date : "1-02-2026",difficulty : "Easy"},
-      {date : "1-02-2026",difficulty : "Easy"},
-      {date : "1-02-2026",difficulty : "Easy"},
-      {date : "1-02-2026",difficulty : "Easy"},
-      {date : "1-02-2026",difficulty : "Easy"},
-      {date : "1-02-2026",difficulty : "Easy"},
-      {date : "1-02-2026",difficulty : "hard"},
-      {date : "1-02-2026",difficulty : "hard"},
-      {date : "1-02-2026",difficulty : "hard"},
-      
+  { date: "2026-02-01", difficulty: "easy" },
+  { date: "2026-02-02", difficulty: "easy" },
+  { date: "2026-02-03", difficulty: "easy" },
+  { date: "2026-02-04", difficulty: "easy" },
+  { date: "2026-02-05", difficulty: "easy" },
+  { date: "2026-02-06", difficulty: "easy" },
+  { date: "2026-02-07", difficulty: "easy" },
+  { date: "2026-02-08", difficulty: "easy" },
+  { date: "2026-02-09", difficulty: "easy" },
+  { date: "2026-02-10", difficulty: "hard" },
+  { date: "2026-02-11", difficulty: "easy" },
+  { date: "2026-02-12", difficulty: "easy" },
+  { date: "2026-02-13", difficulty: "hard" },
+  { date: "2026-02-14", difficulty: "easy" },
+  { date: "2026-02-15", difficulty: "hard" },
+  { date: "2026-02-16", difficulty: "easy" },
+  { date: "2026-02-17", difficulty: "easy" },
+  { date: "2026-02-18", difficulty: "easy" },
+  { date: "2026-02-18", difficulty: "easy" },
+  { date: "2026-02-18", difficulty: "hard" },
+  { date: "2026-02-18", difficulty: "hard" },
+  { date: "2026-02-19", difficulty: "easy" },
+  { date: "2026-02-19", difficulty: "easy" },
+  { date: "2026-02-20", difficulty: "easy" },
+  { date: "2026-02-21", difficulty: "easy" },
+  { date: "2026-02-22", difficulty: "easy" },
+  { date: "2026-02-23", difficulty: "easy" },
+  { date: "2026-02-24", difficulty: "easy" },
+  { date: "2026-02-25", difficulty: "easy" },
+  { date: "2026-02-25", difficulty: "easy" },
+  { date: "2026-02-25", difficulty: "easy" },
 ]
-
 export default problemsData
 
 
@@ -97,3 +96,6 @@ export default problemsData
 // 🔥 Simple Memory Trick:
 // Named export  → { } needed
 // Default export → NO { }
+
+
+// JavaScript date format is YYYY-MM-DD
