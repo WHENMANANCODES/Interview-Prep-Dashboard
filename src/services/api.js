b92fff0e86bb912873deebe89e4ff0f9a4fe1b9c
WHENMANANCODES <mananjain564@gmail.com>
2026-06-08 00:17:00 +0530

    fix api url

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://your-render-url/api/problems';
+const API_URL = 'https://interview-prep-dashboard-2.onrender.com/api/problems';
 
 export const getProblems = async () => {
   const response = await axios.get(API_URL);
